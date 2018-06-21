37daa2638503a39a8f1c06f066082f24877a0f2c
speake <samp97@vt.edu>
2018-06-21 11:12:15 -0400

    Added Calendar body, header, and month changer.

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,11 +1,21 @@
 import Vue from 'vue';
 import './style.scss';
 
+import store from './store';
+
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', { get: function() { return this.$root.moment; }});
+
 import App from './components/App.vue';
 
 new Vue({
   el: '#app',
+  data: {
+    moment
+  },
   components: {
     App
-  }
+  },
+  store
 });
